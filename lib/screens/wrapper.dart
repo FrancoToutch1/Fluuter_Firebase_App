2af5d6165480c72167409d1bdbb2936303e5d5ff
Franc TOUTCHA <arnauld.toutcha@facsciences-uy1.cm>
2023-05-02 14:54:35 +0100

    firebase setup

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/screens/home/home.dart';
+import 'package:flutter_firebase_app/screens/authenticate/authenticate.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Home();
+  }
+}
+
